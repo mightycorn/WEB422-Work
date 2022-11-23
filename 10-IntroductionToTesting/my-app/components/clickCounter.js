6e9212f75ac0275045b29020bbc847ea57c47385
Alfie <57643621+mightycorn@users.noreply.github.com>
2022-11-23 02:56:39 -0500

    WIP testing

---
+++ 10-IntroductionToTesting/my-app/components/clickCounter.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export default function ClickCounter() {
+  const [count, setCount] = useState(0);
+  return (
+    <button
+      onClick={() => {
+        setCount((count) => count + 1);
+      }}
+    >
+      Clicked {count} Times
+    </button>
+  );
+}
